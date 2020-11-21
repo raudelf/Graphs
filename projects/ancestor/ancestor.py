202c6d6400c562f2aa1b42ac6129856d21895580
raudelf <rawdell@outlook.com>
2020-11-20 22:59:10 -0600

    :white_check_mark: LESSONS
    Completed most of the assignments, need to finish ancestors tho

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -1,3 +1,31 @@
+from util import Stack, Queue
+# def dft(ancestors, starting_node, distance):
 
-def earliest_ancestor(ancestors, starting_node):
+#     parents = getNeighbors(node)
+
+#     if len(parents) == 0:
+#         return (node, distance)
+
+#     ancient_one = (node, distance)
+#     for parent in parents:
+#         node_pair = dft(ancestors, parent, distance + 1)
+
+#         if node_pair[1] > distance:
+#             ancient_one = node_pair
+
+
+def getNeighbors(ancestors, curr_node):
+    neighbors = []
+
+    for neighbor in ancestors:
+        if neighbor[1] == curr_node:
+            neighbors.append(neighbor)
+
+        if len(neighbors) > 1:
+            return neighbors
+        else:
+            return neighbors
+
+
+def earliest_ancestor(ancestors, starting_node, distance=0):
     pass
--- projects/social/social.py
+++ projects/social/social.py
@@ -1,6 +1,13 @@
+import random
+
+from util import Stack, Queue
+from collections import deque
+
+
 class User:
     def __init__(self, name):
         self.name = name
+
 
 class SocialGraph:
     def __init__(self):
@@ -28,6 +35,14 @@ class SocialGraph:
         self.users[self.last_id] = User(name)
         self.friendships[self.last_id] = set()
 
+    def fisher_yates_shuffle(self, l):
+        for i in range(0, len(l)):
+            random_index = random.randint(i, len(l) - 1)
+            l[random_index], l[i] = l[i], l[random_index]
+
+    def get_friends(self, curr_friend):
+        return self.friendships[curr_friend]
+
     def populate_graph(self, num_users, avg_friendships):
         """
         Takes a number of users and an average number of friendships
@@ -45,8 +60,25 @@ class SocialGraph:
         # !!!! IMPLEMENT ME
 
         # Add users
-
+        for u in range(0, num_users):
+            self.add_user(f'User{u + 1}')
         # Create friendships
+        # if 1 is a friend of 2, and 2 is a friend of 1, count this as 2 friendships
+        total_friendships = avg_friendships * num_users
+
+        # create a list with all possible friendship combinations,
+        # friendship_combos = [(0, 1), (0, 2), (0, 3), (1, 2), (1, 3), (2, 3)]
+        friendship_combos = []
+        for user_id in range(1, num_users + 1):
+            # You can avoid this by only creating friendships where user1 < user2
+            for friend_id in range(user_id + 1, num_users + 1):
+                friendship_combos.append((user_id, friend_id))
+        # shuffle the list,
+        self.fisher_yates_shuffle(friendship_combos)
+    # then grab the first N elements from the list
+        friendships_to_make = friendship_combos[:(total_friendships // 2)]
+        for friendship in friendships_to_make:
+            self.add_friendship(friendship[0], friendship[1])
 
     def get_all_social_paths(self, user_id):
         """
@@ -57,8 +89,30 @@ class SocialGraph:
 
         The key is the friend's ID and the value is the path.
         """
+        q = Queue()
         visited = {}  # Note that this is a dictionary, not a set
         # !!!! IMPLEMENT ME
+        while q.size() > 0:
+
+            # get the next person in line
+            curr_path = q.dequeue()
+            curr_person = curr_path[-1]
+
+            # Check if we visited them yet
+            if curr_person not in visited:
+                # If not, mark as visited
+                # Key: user_id, Value: path
+                visited[curr_person] = curr_path
+                # Get their friends (visited their edges)
+                friends = self.get_friends(curr_person)
+
+                # Enqueue them
+                for friend in friends:
+                    friend_path = list(curr_path)
+
+                    friend_path.append(friend)
+
+                    q.enqueue(friend_path)
         return visited
 
 
